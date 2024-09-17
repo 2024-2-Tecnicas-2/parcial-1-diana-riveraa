6498f0de3f08bc760108940ec002991d6e03449a
limon <kilo@gmail.com>
2024-09-16 20:45:04 -0500

    Solución

--- src/main/java/com/mycompany/parcial1/Principal.java
+++ src/main/java/com/mycompany/parcial1/Principal.java
@@ -2,7 +2,25 @@ package com.mycompany.parcial1;
 
 public class Principal {
 
-    public static void main(String[] args) {
-        // TODO: Aquí ingresas tu código para iniciar la aplicación.
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int cantidadInventario;
+
+    public Producto(String nombre, String descripcion, double precio, int cantidadInventario) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidadInventario = cantidadInventario;
+    }
+    public String getNombre() { return nombre; }
+    public String getDescripcion() { return descripcion; }
+    public double getPrecio() { return precio; }
+    public int getCantidadInventario() { return cantidadInventario; }
+    public void setCantidadInventario(int cantidad) {
+        this.cantidadInventario = cantidad;
+    }
+    public void mostrarInformacion() {
+        System.out.println("Producto: " + nombre + ", Precio: " + precio + ", Inventario: " + cantidadInventario);
     }
 }
